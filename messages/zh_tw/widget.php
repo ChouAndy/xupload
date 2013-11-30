93c8e88c39a05f9d22f1cd8e06a37cd9a922bbd9
ChouAndy <chouandy625@gmail.com>
2013-12-01 04:00:32 +0800

    Modify code.

---
+++ messages/zh_tw/widget.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    '1#Add files|0#Choose file' => '1#新增檔案|0#選擇檔案',
+    'Start upload' => '開始上傳',
+    'Cancel upload' => '取消上傳',
+    'File is too big' => '檔案太大',
+    'File is too small' => '檔案太小',
+    'Filetype not allowed' => '檔案格式不允許',
+    'Max number of files exceeded' => 'Max number of files exceeded',
+    'Uploaded bytes exceed file size' => 'Uploaded bytes exceed file size',
+    'Empty file upload result' => 'Empty file upload result',
+    "Error" => '錯誤',
+    "Start" => '開始',
+    "Cancel" => '取消',
+    "Delete" => '刪除',
+);
